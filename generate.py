ffbb3f17a30feb15e19b99734784a0c9ee939ade
Jcheck27 <jordan.checkoff@gmail.com>
2023-01-08 00:45:39 -0600

    CreateWorld()

--- generate.py
+++ generate.py
@@ -1,17 +1,19 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("world.sdf")
 
-length = 1
-width = 1
-height = 1
+def CreateWorld():
+  pyrosim.Start_SDF("world.sdf")
 
-x = 0
-y = 0
-z = 0.5
+  length = 1
+  width = 1
+  height = 1
+  x = 0
+  y = 0
+  z = 0.5
+
+  pyrosim.Send_Cube(name="Box", pos=[x,y,z], size=[length,width,height])
+
+  pyrosim.End()
 
 
-pyrosim.Send_Cube(name="Box", pos=[x,y,z], size=[length,width,height])
-
-
-pyrosim.End()
+CreateWorld()
