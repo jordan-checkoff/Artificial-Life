df31c5233c419568259a2bdf01a8311d35ac28bf
Jcheck27 <jordan.checkoff@gmail.com>
2023-01-08 01:05:40 -0600

    first connection

--- generate.py
+++ generate.py
@@ -1,19 +1,21 @@
 import pyrosim.pyrosim as pyrosim
 
 
-def CreateWorld():
+def Create_World():
   pyrosim.Start_SDF("world.sdf")
-
-  length = 1
-  width = 1
-  height = 1
-  x = 0
-  y = 0
-  z = 0.5
-
-  pyrosim.Send_Cube(name="Box", pos=[x,y,z], size=[length,width,height])
-
+  pyrosim.Send_Cube(name="Box", pos=[-5,5,0.5], size=[1,1,1])
   pyrosim.End()
 
 
-CreateWorld()
+def Create_Robot():
+  pyrosim.Start_URDF("body.urdf")
+  pyrosim.Send_Cube(name="Link0", pos=[0,0,0.5], size=[1,1,1])
+  pyrosim.Send_Joint( name = "Link0_Link1" , parent= "Link0" , child = "Link1" , type = "revolute", position = [0,0,1])
+  pyrosim.Send_Cube(name="Link1", pos=[0,0,0.5], size=[1,1,1])
+  pyrosim.End()
+
+
+
+Create_World()
+
+Create_Robot()
